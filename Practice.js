e495709022d9dbb784643a958738c992c1abc7d2
pratyushashukla <pratyusha.shukla212@gmail.com>
2024-05-24 12:12:19 -0400

    first commit

---
+++ Practice.js
@@ -0,0 +1,12 @@
+//functions with no arguments and return 
+function showCity() 
+{ 
+    console.log("Toronto"); 
+    showCountry(); 
+}
+function showCountry() 
+{
+    console.log("Canada"); 
+} 
+
+showCity();
